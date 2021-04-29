0bc02d5a8d121c5fa7f3f0dba0a640495651dcb2
Jon Greazel <jgreazel@dmsi.com>
2021-04-29 09:16:57 -0500

    change

--- graphql-practice/src/App.js
+++ graphql-practice/src/App.js
@@ -14,16 +14,38 @@ const GET_LAUNCHES = gql`
   }
 `;
 
+const GET_SHIPS = gql`
+  {
+    ships(limit: 10) {
+      model
+      name
+      roles
+      year_built
+    }
+  }
+`;
+
 function App() {
-  const { loading, error, data } = useQuery(GET_LAUNCHES);
+  const { loading, error, data } = useQuery(GET_SHIPS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Whoops.. something is wrong</p>;
 
   return (
     <>
-      <h2>Github</h2>
-      {console.log(data)}
+      <h2>Spacex Ships</h2>
+      {data.ships.map((ship) => (
+        <div>
+          <h4>{ship.name}</h4>
+          <p>Built: {ship.year_built}</p>
+          <p>
+            Roles:{" "}
+            {ship.roles.map((role) => (
+              <li>{role}</li>
+            ))}
+          </p>
+        </div>
+      ))}
     </>
   );
 }
